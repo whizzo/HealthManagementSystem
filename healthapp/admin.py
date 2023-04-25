203d2a16e4fb31f52cc45703c25d4ce0ffae9d58
whizzo <albosg10@gmail.com>
2023-04-25 22:23:40 +0200

    commit changes
    Added Templates folder, Added Views, and urls for Appointment, registered models in app/admin

--- healthapp/admin.py
+++ healthapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Patient, Doctor, Appointment, Billing
+
+
+admin.site.register(Patient)
+admin.site.register(Doctor)
+admin.site.register(Appointment)
+admin.site.register(Billing)
+
 
 # Register your models here.
--- healthapp/views.py
+++ healthapp/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Appointment, Patient, Doctor, Billing
+
+
+class AppointmentListView(ListView):
+    model = Appointment
+    template_name = "appointment.html"
+
+class PatientListView(ListView):
+    model = Patient
+    template_name = 'patient.html'
+
+class DoctorListView(ListView):
+    model = Doctor
+    template_name = 'doctor.html'
+
+class BillingListView(ListView):
+    model = Billing
+    template_name = 'billing.html'
+
 
 # Create your views here.
